a88354a26a674923507151979513e91b393d377b
Peter Bamidele <peterbamidele629@gmail.com>
2026-02-10 21:34:48 +0100

    Final commit

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "Public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index");
 });
 
 app.post("/", async (req, res) => {
@@ -19,10 +26,10 @@ app.post("/", async (req, res) => {
     );
     const result = response.data;
     console.log(result);
-    res.render("index.ejs", { data: result });
+    res.render("index", { data: result });
   } catch (error) {
     console.error("Failed to make request:", error.message);
-    res.render("index.ejs", { error: error.message });
+    res.render("index", { error: error.message });
   }
 });
 
